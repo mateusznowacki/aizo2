d37551f4efecee8aeff7865f9ef4c71c5726f839
Mateusz <mattynovy@outlook.com>
2024-06-13 11:23:54 +0200

    finished consolea

--- service/Controller.cpp
+++ service/Controller.cpp
@@ -258,7 +258,131 @@ void Controller::generateOwnDataAndSort() {
 }
 
 void Controller::getDataFromFileAndSort() {
+    bool printArray = console.askIfPrintArray();
 
+    int choice ;
+
+    Graph *dirGraph = fileManag.loadGraphFromFile(console.printGetFilename());
+    Graph *unDirGraph = dataGenerator.generateUnDirGraph(dirGraph);
+    chrono::high_resolution_clock::time_point start, end;
+
+    if(printArray) {
+        cout << "Macierz incydencji dla grafu skierowanego " << endl;
+        dirGraph->printAdjMatrix(printArray);
+        cout << "Lista sąsiedztwa dla grafu skierowanego " << endl;
+        dirGraph->printAdjList(printArray);
+        cout << "Macierz incydencji dla grafu nieskierowanego " << endl;
+        unDirGraph->printAdjMatrix(printArray);
+        cout << "Lista sąsiedztwa dla grafu nieskierowanego " << endl;
+        unDirGraph->printAdjList(printArray);
+    }
+
+    do {
+        choice = console.printSortingAlgorithmsOptions();
+        switch (choice) {
+
+            case 1: //mst
+            {
+                int mstChoice = console.printMSTAlgorithmsOptions();
+                int reprezentationType = console.getTypeOptions();
+                if (mstChoice == 1) {
+                    if (reprezentationType == 2) {
+                        start = chrono::high_resolution_clock::now();
+                        algorithms.primMSTMatrix(*unDirGraph);
+                        end = chrono::high_resolution_clock::now();
+                        chrono::microseconds duration = chrono::duration_cast<chrono::microseconds>(end - start);
+                        cout << "Czas trwania: " << duration.count() << " mikrosekund" << endl;
+                        break;
+                    } else if (reprezentationType == 1) {
+                        start = chrono::high_resolution_clock::now();
+                        algorithms.primMSTList(*unDirGraph);
+                        end = chrono::high_resolution_clock::now();
+                        chrono::microseconds duration = chrono::duration_cast<chrono::microseconds>(end - start);
+                        cout << "Czas trwania: " << duration.count() << " mikrosekund" << endl;
+                        break;
+                    }
+
+                } else if (mstChoice == 2) {
+                    if (reprezentationType == 2) {
+                        start = chrono::high_resolution_clock::now();
+                        algorithms.kruskalMSTList(*unDirGraph);
+                        end = chrono::high_resolution_clock::now();
+                        chrono::microseconds duration = chrono::duration_cast<chrono::microseconds>(end - start);
+                        cout << "Czas trwania: " << duration.count() << " mikrosekund" << endl;
+                        break;
+                    } else if (reprezentationType == 1) {
+                        start = chrono::high_resolution_clock::now();
+                        algorithms.kruskalMSTMatrix(*unDirGraph);
+                        end = chrono::high_resolution_clock::now();
+                        chrono::microseconds duration = chrono::duration_cast<chrono::microseconds>(end - start);
+                        cout << "Czas trwania: " << duration.count() << " mikrosekund" << endl;
+                        break;
+                    }
+
+                }
+            }
+            case 2: //sciezka
+
+            {  int pathChoice = console.printShortPathAlgorithmsOptions();
+                int reprezentationType = console.getTypeOptions();
+                int *vertices = console.getVerticesToPath();
+                if (pathChoice == 1) {
+                    if (reprezentationType == 1) {
+                        start = chrono::high_resolution_clock::now();
+                        algorithms.dijkstraList(*dirGraph, vertices[0], vertices[1],true);
+                        end = chrono::high_resolution_clock::now();
+                        chrono::microseconds duration = chrono::duration_cast<chrono::microseconds>(end - start);
+                        cout << "Czas trwania: " << duration.count() << " mikrosekund" << endl;
+                        break;
+                    } else if (reprezentationType == 2) {
+                        start = chrono::high_resolution_clock::now();
+                        algorithms.dijkstraMatrix(*dirGraph, vertices[0], vertices[1],true);
+                        end = chrono::high_resolution_clock::now();
+                        chrono::microseconds duration = chrono::duration_cast<chrono::microseconds>(end - start);
+                        cout << "Czas trwania: " << duration.count() << " mikrosekund" << endl;
+                        break;
+                    }
+                } else if (pathChoice == 2) {
+                    if (reprezentationType == 2) {
+                        start = chrono::high_resolution_clock::now();
+                        algorithms.bellmanFordMatrix(*dirGraph, vertices[0], vertices[1],true);
+                        end = chrono::high_resolution_clock::now();
+                        chrono::microseconds duration = chrono::duration_cast<chrono::microseconds>(end - start);
+                        cout << "Czas trwania: " << duration.count() << " mikrosekund" << endl;
+                        break;
+                    } else if (reprezentationType == 1) {
+                        start = chrono::high_resolution_clock::now();
+                        algorithms.bellmanFordList(*dirGraph, vertices[0], vertices[1],true);
+                        end = chrono::high_resolution_clock::now();
+                        chrono::microseconds duration = chrono::duration_cast<chrono::microseconds>(end - start);
+                        cout << "Czas trwania: " << duration.count() << " mikrosekund" << endl;
+                        break;
+                    }
+                }
+
+            }
+            case 3: //maxflow
+            {
+                int *vertices = console.getVerticesToPath();
+                int reprezentationType = console.getTypeOptions();
+                if (reprezentationType == 2) {
+                    start = chrono::high_resolution_clock::now();
+                    algorithms.fordFulkersonMatrix(*dirGraph, vertices[0], vertices[1], true);
+                    end = chrono::high_resolution_clock::now();
+                    chrono::microseconds duration = chrono::duration_cast<chrono::microseconds>(end - start);
+                    cout << "Czas trwania: " << duration.count() << " mikrosekund" << endl;
+                    break;
+                } else if (reprezentationType == 1) {
+                    start = chrono::high_resolution_clock::now();
+                    algorithms.fordFulkersonList(*dirGraph, vertices[0], vertices[1], true);
+                    end = chrono::high_resolution_clock::now();
+                    chrono::microseconds duration = chrono::duration_cast<chrono::microseconds>(end - start);
+                    cout << "Czas trwania: " << duration.count() << " mikrosekund" << endl;
+                    break;
+                }
+            }
+        }
+    } while (console.askIfWantToCheckOtherAlgorithm());
 }
 
 
